928c8ee170171041f0de5f7001efe90f18c210ff
Garrett Smith <ggasmithh@users.noreply.github.com>
2020-01-31 13:16:43 -0600

    Initial commit

---
+++ base_project/base_project.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,83 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# app_childtask configuration
+CND_ARTIFACT_DIR_app_childtask=dist/app_childtask/production
+CND_ARTIFACT_NAME_app_childtask=base_project.X.production.hex
+CND_ARTIFACT_PATH_app_childtask=dist/app_childtask/production/base_project.X.production.hex
+CND_PACKAGE_DIR_app_childtask=${CND_DISTDIR}/app_childtask/package
+CND_PACKAGE_NAME_app_childtask=baseproject.x.tar
+CND_PACKAGE_PATH_app_childtask=${CND_DISTDIR}/app_childtask/package/baseproject.x.tar
+# app_ds1631 configuration
+CND_ARTIFACT_DIR_app_ds1631=dist/app_ds1631/production
+CND_ARTIFACT_NAME_app_ds1631=base_project.X.production.hex
+CND_ARTIFACT_PATH_app_ds1631=dist/app_ds1631/production/base_project.X.production.hex
+CND_PACKAGE_DIR_app_ds1631=${CND_DISTDIR}/app_ds1631/package
+CND_PACKAGE_NAME_app_ds1631=baseproject.x.tar
+CND_PACKAGE_PATH_app_ds1631=${CND_DISTDIR}/app_ds1631/package/baseproject.x.tar
+# app_ds1722 configuration
+CND_ARTIFACT_DIR_app_ds1722=dist/app_ds1722/production
+CND_ARTIFACT_NAME_app_ds1722=base_project.X.production.hex
+CND_ARTIFACT_PATH_app_ds1722=dist/app_ds1722/production/base_project.X.production.hex
+CND_PACKAGE_DIR_app_ds1722=${CND_DISTDIR}/app_ds1722/package
+CND_PACKAGE_NAME_app_ds1722=baseproject.x.tar
+CND_PACKAGE_PATH_app_ds1722=${CND_DISTDIR}/app_ds1722/package/baseproject.x.tar
+# app_echo1 configuration
+CND_ARTIFACT_DIR_app_echo1=dist/app_echo1/production
+CND_ARTIFACT_NAME_app_echo1=base_project.X.production.hex
+CND_ARTIFACT_PATH_app_echo1=dist/app_echo1/production/base_project.X.production.hex
+CND_PACKAGE_DIR_app_echo1=${CND_DISTDIR}/app_echo1/package
+CND_PACKAGE_NAME_app_echo1=baseproject.x.tar
+CND_PACKAGE_PATH_app_echo1=${CND_DISTDIR}/app_echo1/package/baseproject.x.tar
+# app_flashled configuration
+CND_ARTIFACT_DIR_app_flashled=dist/app_flashled/production
+CND_ARTIFACT_NAME_app_flashled=base_project.X.production.hex
+CND_ARTIFACT_PATH_app_flashled=dist/app_flashled/production/base_project.X.production.hex
+CND_PACKAGE_DIR_app_flashled=${CND_DISTDIR}/app_flashled/package
+CND_PACKAGE_NAME_app_flashled=baseproject.x.tar
+CND_PACKAGE_PATH_app_flashled=${CND_DISTDIR}/app_flashled/package/baseproject.x.tar
+# app_irqs configuration
+CND_ARTIFACT_DIR_app_irqs=dist/app_irqs/production
+CND_ARTIFACT_NAME_app_irqs=base_project.X.production.hex
+CND_ARTIFACT_PATH_app_irqs=dist/app_irqs/production/base_project.X.production.hex
+CND_PACKAGE_DIR_app_irqs=${CND_DISTDIR}/app_irqs/package
+CND_PACKAGE_NAME_app_irqs=baseproject.x.tar
+CND_PACKAGE_PATH_app_irqs=${CND_DISTDIR}/app_irqs/package/baseproject.x.tar
+# app_reverse configuration
+CND_ARTIFACT_DIR_app_reverse=dist/app_reverse/production
+CND_ARTIFACT_NAME_app_reverse=base_project.X.production.hex
+CND_ARTIFACT_PATH_app_reverse=dist/app_reverse/production/base_project.X.production.hex
+CND_PACKAGE_DIR_app_reverse=${CND_DISTDIR}/app_reverse/package
+CND_PACKAGE_NAME_app_reverse=baseproject.x.tar
+CND_PACKAGE_PATH_app_reverse=${CND_DISTDIR}/app_reverse/package/baseproject.x.tar
+# app_semaphore1 configuration
+CND_ARTIFACT_DIR_app_semaphore1=dist/app_semaphore1/production
+CND_ARTIFACT_NAME_app_semaphore1=base_project.X.production.hex
+CND_ARTIFACT_PATH_app_semaphore1=dist/app_semaphore1/production/base_project.X.production.hex
+CND_PACKAGE_DIR_app_semaphore1=${CND_DISTDIR}/app_semaphore1/package
+CND_PACKAGE_NAME_app_semaphore1=baseproject.x.tar
+CND_PACKAGE_PATH_app_semaphore1=${CND_DISTDIR}/app_semaphore1/package/baseproject.x.tar
+# app_semaphore2 configuration
+CND_ARTIFACT_DIR_app_semaphore2=dist/app_semaphore2/production
+CND_ARTIFACT_NAME_app_semaphore2=base_project.X.production.hex
+CND_ARTIFACT_PATH_app_semaphore2=dist/app_semaphore2/production/base_project.X.production.hex
+CND_PACKAGE_DIR_app_semaphore2=${CND_DISTDIR}/app_semaphore2/package
+CND_PACKAGE_NAME_app_semaphore2=baseproject.x.tar
+CND_PACKAGE_PATH_app_semaphore2=${CND_DISTDIR}/app_semaphore2/package/baseproject.x.tar
+# app_timerLEDecho configuration
+CND_ARTIFACT_DIR_app_timerLEDecho=dist/app_timerLEDecho/production
+CND_ARTIFACT_NAME_app_timerLEDecho=base_project.X.production.hex
+CND_ARTIFACT_PATH_app_timerLEDecho=dist/app_timerLEDecho/production/base_project.X.production.hex
+CND_PACKAGE_DIR_app_timerLEDecho=${CND_DISTDIR}/app_timerLEDecho/package
+CND_PACKAGE_NAME_app_timerLEDecho=baseproject.x.tar
+CND_PACKAGE_PATH_app_timerLEDecho=${CND_DISTDIR}/app_timerLEDecho/package/baseproject.x.tar
+# esos_skel configuration
+CND_ARTIFACT_DIR_esos_skel=dist/esos_skel/production
+CND_ARTIFACT_NAME_esos_skel=base_project.X.production.hex
+CND_ARTIFACT_PATH_esos_skel=dist/esos_skel/production/base_project.X.production.hex
+CND_PACKAGE_DIR_esos_skel=${CND_DISTDIR}/esos_skel/package
+CND_PACKAGE_NAME_esos_skel=baseproject.x.tar
+CND_PACKAGE_PATH_esos_skel=${CND_DISTDIR}/esos_skel/package/baseproject.x.tar
---
+++ base_project/main.c
@@ -0,0 +1,14 @@
+/*
+ * File:   main.c
+ * Author: Garrett
+ *
+ * Created on January 31, 2020, 1:13 PM
+ */
+
+
+#include "pic24_all.h"
+#include "esos.h"
+
+int main(void) {
+    return 0;
+}
